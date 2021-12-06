578d13fd7b5e493950d9f4c725e6a7fe576eca46
flipeprez <flipeprez@gmail.com>
2021-12-06 08:10:28 -0800

    new libshell update

--- libshell.h
+++ libshell.h
@@ -46,4 +46,5 @@ path_d *add_node_pathend(path_d **head, char *str);
 tok *add_node_tokens(tok **head, char *str);
 tok *add_node_end(tok **head, char *str);
 char *_strdup(char *str);
+char *_strncat(char *dest, char *src, int n);
 #endif
